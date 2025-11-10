2c8392b90d8ab25ad388f40899a715c9594c68b5
Arian <arian.peymani@gmail.com>
2025-11-10 21:48:10 +0330

    jQuery added for LOGO

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#logo").mouseenter(function(){
+        $("#logo code").text("arian")
+    });
+     $("#logo").mouseleave(function(){
+        $("#logo code").text("a")
+    });
+});
